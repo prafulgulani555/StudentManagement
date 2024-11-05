98d3f78116bc2b44381aaa87909d92eaa75ce46b
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-11-05 20:02:45 +0530

    creating a window and table

---
+++ main.py
@@ -0,0 +1,31 @@
+from wsgiref.util import application_uri
+
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
+    QTableWidget
+from PyQt6.QtGui import QAction
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+
+app = QApplication(sys.argv)
+student_management = MainWindow()
+student_management.show()
+sys.exit(app.exec())
